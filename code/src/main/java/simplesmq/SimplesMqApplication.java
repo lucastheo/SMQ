2709bfcf7a44c4a9f5385682150db51d774541f7
L Theodoro G A <lucastheo.g.a@gmail.com>
2020-12-16 11:11:03 -0300

    Commit inicial

---
+++ code/src/main/java/simplesmq/SimplesMqApplication.java
@@ -0,0 +1,13 @@
+package simplesmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesMqApplication.class, args);
+	}
+
+}
---
+++ code/src/test/java/simplesmq/SimplesMqApplicationTests.java
@@ -0,0 +1,13 @@
+package simplesmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
